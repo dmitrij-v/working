c19a70166601b6a941107ec4da9428f9ab9e0886
Dmitrij <d.verenikin@dunice.net>
2020-01-23 15:44:12 +0300

    Added recipe show page

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -2,5 +2,7 @@ class RecipiesController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @recipe = Recipe.find_by(id: params[:id])
+    return redirect_to :root, alert: "I'm soo sorry, but recipe not found." unless @recipe
   end
 end

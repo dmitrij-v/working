dc1e469f169f2c09aa1eee1d8f3138f443ec42cc
Dmitrij <d.verenikin@dunice.net>
2020-01-23 16:22:14 +0300

    Steps were added to recipe

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -2,7 +2,7 @@ class RecipiesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @recipe = Recipe.find_by(id: params[:id])
+    @recipe = Recipe.includes(:steps).find_by(id: params[:id])
     return redirect_to :root, alert: "I'm soo sorry, but recipe not found." unless @recipe
   end
 end

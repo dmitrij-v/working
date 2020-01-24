817e3dce78b577bfdf4b84fcd425589d99039ed1
Dmitrij <d.verenikin@dunice.net>
2020-01-24 11:16:23 +0300

    Added links to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
-  def show_link(user)
+  def login_link(user)
     return link_to 'Log out', destroy_user_session_path, method: 'delete' if user
     link_to 'Login', new_user_session_path
+  end
+
+  def profile_link(user)
+    return link_to 'Profile', profile_path if user
+    nil
   end
 end

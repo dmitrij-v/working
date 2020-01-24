b66e9d2612bcf630e135fc014f057f8ad3a50e85
Dmitrij <d.verenikin@dunice.net>
2020-01-24 15:11:54 +0300

    Create recipe parcer

--- app/services/recipe_parcer.rb
+++ app/services/recipe_parcer.rb
@@ -4,7 +4,17 @@ class RecipeParcer
   end
 
   def parce
-    recipe_hash = {
+    recipe = create_recipe(parce_recipe)
+    create_steps(parce_steps, recipe)
+    create_ingredients(parce_ingredients, recipe)
+
+    recipe
+  end
+
+  private
+
+  def parce_recipe
+    {
       api_id:           @text['id'],
       title:            @text['title'],
       time_for_cooking: @text['readyInMinutes'],
@@ -13,7 +23,10 @@ class RecipeParcer
       image_link:       @text['image'],
       original_source:  @text['sourceUrl']
     }
-    steps = @text['analyzedInstructions'].map do |big_step|
+  end
+
+  def parce_steps
+    @text['analyzedInstructions'].map do |big_step|
       big_step['steps'].map do |step|
         {
           number:      step['number'],
@@ -21,14 +34,46 @@ class RecipeParcer
         }
       end
     end
-    ingredients = @text['extendedIngredients'].map do |ingredient|
+  end
+
+  def parce_ingredients
+    @text['extendedIngredients'].map do |ingredient|
       {
-        type:             ingredient['aisle'],
-        title:            ingredient['name'],
-        full_description: ingredient['originalString'],
-        consitency:       ingredient['consitency']
+        type:       ingredient['aisle'],
+        ingredient: {
+          title:            ingredient['name'],
+          full_description: ingredient['originalString'],
+          consitency:       ingredient['consitency']
+        }
       }
     end
-    puts ingredients
+  end
+
+  def create_recipe(data_hash)
+    old = Recipe.find_by(api_id: data_hash[:api_id])
+    return old if old
+
+    Recipe.create(data_hash)
+  end
+
+  def create_steps(steps_array, recipe)
+    steps_array.flatten.map do |step|
+      old_step = recipe.steps.find_by(description: step[:description])
+      return old_step if old_step
+
+      recipe.steps.create(step)
+    end
+  end
+
+  def create_ingredients(ingredients_array, recipe)
+    ingredients_array.flatten.map do |ingredient|
+      old_ingredient = recipe.ingredients.find_by(title: ingredient[:ingredient][:title])
+      return old_ingredient if old_ingredient
+
+      type = IngredientCategory.find_or_create_by_(title: ingredient[:type])
+
+      ingredient[:ingredient][:ingredient_category_id] = type.id
+      recipe.ingredients.create(ingredient[:ingredient])
+    end
   end
 end

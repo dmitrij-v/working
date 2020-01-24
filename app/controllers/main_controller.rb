dd3655da4d4f3a94b766924aab0ee96d315c8d27
Dmitrij <d.verenikin@dunice.net>
2020-01-24 17:29:25 +0300

    Reorder main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,6 @@ class MainController < ApplicationController
   before_action :authenticate_user!, except: [:main]
 
   def main
-    @recipies = Recipe.last(10)
+    @recipies = Recipe.last(10).reverse
   end
 end

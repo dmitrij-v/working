40ebca816c21b0b92443fe0b9f64e2fa6709dd8e
Dmitrij <d.verenikin@dunice.net>
2020-01-27 14:49:58 +0300

    Added steps to recipe edit form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ApplicationRecord
   has_many :steps, foreign_key: 'recipe_id', class_name: 'RecipeStep'
   has_many :ingredients
 
+  accepts_nested_attributes_for "steps"
+
   scope :shown, -> { where(public: true) }
   scope :hidden, -> { where(public: false) }
 end
